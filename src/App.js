8b45b955fdd9f2ecc0c0449659b74924d4125dc4
sblobs <sonsun.dev@gmail.com>
2022-03-31 00:39:48 +1100

    Added starry background using tsparticles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Particles from 'react-tsparticles';
 
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
@@ -10,7 +11,76 @@ import NotFoundPage from './pages/404';
 
 const App = () => {
   const location = useLocation();
-  return (
+  const particleOptions = {
+    fpsLimit: 60,
+    interactivity: {
+      detect_on: 'window',
+      events: {
+        onclick: { enable: true, mode: 'push' },
+        resize: true,
+        onhover: {
+          enable: true,
+          mode: 'connect'
+        },
+      },
+      modes: {
+        bubble: {
+          distance: 40,
+          duration: 2,
+          opacity: 0.7,
+          size: 3,
+          speed: 3
+        },
+        push: {
+          quantity: 1,
+        },
+        connect: {
+          distance: 50,
+          links: {
+            opacity: 0.2
+          },
+          radius: 60
+        },
+        repulse: { distance: 300, duration: 0.1 },
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+      }
+    },
+    particles: {
+      color: {
+        value: '#82d3ff',
+        opacity: 0.6
+      },
+      move: {
+        bounce: false,
+        direction: 'none',
+        enable: true,
+        out_mode: 'out',
+        random: true,
+        speed: 0.2,
+        straight: false
+      },
+      number: { density: { enable: true, value_area: 100 }, value: 13 },
+      opacity: {
+        anim: { enable: true, opacity_min: 0.3, speed: 5, sync: false },
+        random: {
+          enable: true,
+          minimumValue: 0.3
+        },
+        value: 0.6
+      },
+      shape: {
+        type: 'circle'
+      },
+      size: {
+        random: false,
+        value: 1.3
+      }
+    },
+    retina_detect: true
+  };
+
+  return (<>
+    <Particles className='particles' options={particleOptions} />
     <TransitionGroup component={null}>
       <CSSTransition key={location.key} classNames='fade' timeout={300}>
         <Routes location={location}>
@@ -23,6 +93,7 @@ const App = () => {
         </Routes>
       </CSSTransition>
     </TransitionGroup>
+  </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './Home.module.css';
 import MenuBar from '../components/MenuBar';
 const HomePage = () => {
-  return <div className={styles.main}>
+  return <div className={styles.main}> <div className={styles.textBanner}>
     <div className={styles.title}>
       Sonia Sun
     </div>
     <div className={styles.content}>
-      penultimate blah blah
+      Penultimate year computer science/media arts student at UNSW.
     </div>
     <MenuBar />
-  </div>
+  </div></div>
 }
 
 export default HomePage;
