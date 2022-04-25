3fc1663bd4964d1498a29cb0c594bc4600c56dc8
sblobs <sonsun.dev@gmail.com>
2022-04-25 13:41:54 +1000

    Changed favicon

--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -1,5 +1,5 @@
 const About = {
-  desc: "Hi! (＾▽＾) I'm Sonia, a penultimate year computer science and media arts student at UNSW."
+  desc: "Hi! I'm Sonia, a penultimate year computer science and media arts student at UNSW."
 };
 
 export default About;
