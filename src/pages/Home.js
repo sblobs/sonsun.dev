7497d678d5d51528b352a45c9ee319f15185a94f
sblobs <sonsun.dev@gmail.com>
2022-06-01 00:43:22 +1000

    Homepage title & about page changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import MenuBar from '../components/MenuBar';
 const HomePage = () => {
   return <div className={styles.main}>
     <div className={styles.title}>
-      Sonia Sun
+      Hi! I&apos;m Sonia :)
     </div>
     <div className={styles.content}>
       Penultimate year computer science/ media arts student at UNSW.
