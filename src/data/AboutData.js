f957a1b59c2bc11e48bb8443c6036e56acc3753e
sblobs <sonsun.dev@gmail.com>
2022-09-11 19:07:23 +1000

    Changed css to scss and edited about description

--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -1,5 +1,5 @@
 const About = {
-  desc: "I'm a penultimate year computer science and media arts student at UNSW Sydney."
+  desc: "Hi :) I'm Sonia, a penultimate year computer science and media arts student at UNSW. In my spare time, I enjoy knitting and learning to make music."
 };
 
 export default About;
